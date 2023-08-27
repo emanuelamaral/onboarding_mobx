7e1fe82cbf4d4e986014f10a84806670de94fbfd
emanuel <amoz_c.p.t@hotmail.com>
2023-08-27 17:09:06 -0300

    refatoração do código

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'features/onboard_page.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AppWidget> createState() => OnboardPage();
+}
