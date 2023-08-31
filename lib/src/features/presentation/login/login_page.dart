74c1e591777aa1abbe79041b1242c467035163cc
emanuel <amoz_c.p.t@hotmail.com>
2023-08-30 21:54:57 -0300

    Alteração e validação de login

--- lib/src/features/presentation/login/login_page.dart
+++ lib/src/features/presentation/login/login_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:mobx/mobx.dart';
 import 'package:onboarding_mobx/src/features/components/checkbox/checkbox.dart';
+import 'package:onboarding_mobx/src/features/presentation/pass_recovery/pass_recovery.dart';
 import 'package:onboarding_mobx/src/features/presentation/register/register_page.dart';
+import 'package:onboarding_mobx/src/features/presentation/register/stores/register_store.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final emailController = TextEditingController();
+    final senhaController = TextEditingController();
+
+    final loginSuccess = Observable(false);
+
+    RegisterStore registerStore = RegisterStore();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
@@ -14,21 +25,22 @@ class LoginPage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Email'),
-              keyboardType: TextInputType.emailAddress,
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Senha'),
-            ),
-          ),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Email'),
+                keyboardType: TextInputType.emailAddress,
+              )),
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                controller: senhaController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Senha'),
+              )),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
@@ -40,7 +52,7 @@ class LoginPage extends StatelessWidget {
                     TextButton(
                       onPressed: () {
                         Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const RegisterPage()));
+                            builder: (context) => RegisterPage()));
                       },
                       child: const Text(
                         'Cadastrar',
@@ -48,7 +60,10 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const PassRecoveryPage()));
+                      },
                       child: const Text('Esqueci minha senha',
                           style: TextStyle(fontSize: 16)),
                     )
@@ -58,24 +73,42 @@ class LoginPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Container(
-                      decoration: BoxDecoration(
-                        // border: Border.all(),
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: const LinearGradient(
-                          colors: <Color>[
-                            Color(0xFF0D47A1),
-                            Color(0xFF1976D2),
-                            Color(0xFF42A5F5),
-                          ],
+                        decoration: BoxDecoration(
+                          // border: Border.all(),
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: const LinearGradient(
+                            colors: <Color>[
+                              Color(0xFF0D47A1),
+                              Color(0xFF1976D2),
+                              Color(0xFF42A5F5),
+                            ],
+                          ),
                         ),
-                      ),
-                      child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(fontSize: 18, color: Colors.white),
-                          )),
-                    ),
+                        child: Observer(
+                          builder: (_) {
+                            return TextButton(
+                                onPressed: () async {
+                                  final email = emailController.text;
+                                  final senha = senhaController.text;
+
+                                  final loginValido = await registerStore
+                                      .verificarLogin(email, senha);
+
+                                  if (loginValido) {
+                                    loginSuccess.value = true;
+                                    print("deu certo");
+                                  } else {
+                                    loginSuccess.value = false;
+                                    print('Deu ruim');
+                                  }
+                                },
+                                child: const Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      fontSize: 18, color: Colors.white),
+                                ));
+                          },
+                        )),
                     TextButton(
                       onPressed: () {},
                       child: const CheckboxComponent(),
