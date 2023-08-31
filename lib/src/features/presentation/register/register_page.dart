c3b9a58f239fd1b8c8ced7ccdb253f29c2066197
emanuel <amoz_c.p.t@hotmail.com>
2023-08-30 21:55:35 -0300

    Alteração da tela e validação do cadastro

--- lib/src/features/presentation/register/register_page.dart
+++ lib/src/features/presentation/register/register_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'stores/register_store.dart';
 
 class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
+  RegisterPage({super.key});
+
+  final emailController = TextEditingController();
+  final senhaController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final registerStore = RegisterStore();
+
     return Scaffold(
         appBar: AppBar(
           title: const Text('Cadastro'),
@@ -12,37 +20,54 @@ class RegisterPage extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Senha'),
-              ),
-            ),
-            Container(
-                decoration: BoxDecoration(
-                  // border: Border.all(),
-                  borderRadius: BorderRadius.circular(10),
-                  gradient: const LinearGradient(
-                    colors: <Color>[
-                      Color(0xFF0D47A1),
-                      Color(0xFF1976D2),
-                      Color(0xFF42A5F5),
-                    ],
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Observer(
+                  builder: (_) => TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Email'),
+                    keyboardType: TextInputType.emailAddress,
                   ),
+                )),
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Observer(builder: (_) {
+                  return TextField(
+                    obscureText: true,
+                    controller: senhaController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Senha'),
+                  );
+                })),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
                 ),
-                child: TextButton(
-                    onPressed: () {},
-                    child: const Text('Cadastrar',
-                        style: TextStyle(fontSize: 18, color: Colors.white))))
+              ),
+              child: TextButton(
+                  onPressed: () {
+                    final email = emailController.text;
+                    final senha = senhaController.text;
+
+                    registerStore.adicionarEmail(email);
+                    registerStore.adicionarSenha(senha);
+
+                    registerStore.cadastarUsuario().then((_) {
+                      Navigator.of(context).pop();
+                    }).catchError((error) {
+                      print(error);
+                    });
+                  },
+                  child: const Text('Cadastrar',
+                      style: TextStyle(fontSize: 18, color: Colors.white))),
+            )
           ],
         ));
   }
